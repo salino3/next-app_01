6353768dd385ee22b0c33038de2e902f4479638f
Salino3 <flaviobcn012@gmail.com>
2026-05-12 20:08:08 +0200

    fix repeated home route

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import { useShallow } from "zustand/react/shallow";
 import { useAppStore } from "./store/store-provider";
 import ContainerLayout from "./(layouts)/container-layout";
-import HomePage from "./(pods)/home/page";
+import HomePage from "./(pods)/(home)/page";
 
 export default function Home() {
   const { user } = useAppStore(
