8a0b688a1e3f4f261b7fcdf549265a271d2d686f
Salino3 <flaviobcn012@gmail.com>
2026-05-14 14:18:14 +0200

    add new page Todos Form Page

---
+++ app/(pods)/todos-form/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+const TodosFormPage = () => {
+  return (
+    <div
+      data-component="TodosFormPage"
+      className="flex flex-col justify-center items-center gap-2 p-2"
+    >
+      <h1 className="text-center text-3xl">TodosFormPage</h1>
+    </div>
+  );
+};
+
+export default TodosFormPage;
